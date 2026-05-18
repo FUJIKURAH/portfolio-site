d03189b7d7dc815dc909f814a128758b366b2e10
FUJIKURAH <haru.qqq0223@gmail.com>
2026-05-18 22:03:07 +0900

    ビルド

--- static/vite.config.js
+++ static/vite.config.js
@@ -1,5 +1,15 @@
 import { resolve } from "node:path";
+import { readdirSync } from "node:fs";
 import { defineConfig } from "vite";
+
+const worksFiles = readdirSync(resolve(__dirname, "works"))
+  .filter((file) => file.endsWith(".html"))
+  .reduce((acc, file) => {
+    const name = file.replace(".html", "");
+
+    acc[name] = resolve(__dirname, `works/${file}`);
+    return acc;
+  }, {});
 
 export default defineConfig({
   base: "/portfolio-site/",
@@ -7,6 +17,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         home: resolve(__dirname, "index.html"),
+        ...worksFiles,
       },
     },
   },
